cf9c4619c8d1776b43253601f58d9d8ac020ad95
MoS73 <97684622+MoS73@users.noreply.github.com>
2022-01-19 15:52:57 +0100

    Add files via upload

---
+++ Freewall/index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--<link rel="stylesheet" href="reset.css">-->
+    <link rel="stylesheet" href="style.css">
+    <title>Free Wall</title>
+</head>
+<body>
+<div id="container" >
+    <div id="box1" class="darkgreen">
+        <div id="hip">
+          <h1><span class="bigger">F</span>ree Wall</h1>
+          <p>Creating dynamic grid layout without gaps</p>
+        </div>
+        <div id="resp_img">
+          <img src="mobile.jpg">
+        </div>
+    </div>
+    <div id="box2" class="blue">
+        <h2>What is Freewall?</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+        <p><strong>Some examples layout:</strong></p>
+        <section>
+         <figure>
+          <img src="flex.png">
+          <figcaption>
+          <p>Flex layout</p>
+          </figcaption>
+         </figure>
+         <figure>
+          <img src="metro.png">
+          <figcaption>
+          <p>Metro style</p>
+          </figcaption>
+         </figure>
+         <figure>
+          <img src="image.png">
+          <figcaption>
+          <p>Images layout</p>
+          </figcaption>
+         </figure>
+         <figure>
+          <img src="pinterest.png">
+          <figcaption>
+          <p>Pinterest layout</p>
+          </figcaption>
+         </figure>
+        </section>
+        
+    </div>
+    <div id="box3" class="green">
+    <h2>How it works</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+    </div>
+    <div id="box4" class="yellow">
+    <h2>Options</h2>
+    <ul>
+       <li>Accueil</li>
+       <li>Accueil
+           <ul>
+               <li>ops</li>
+               <li>ops</li>
+           </ul>
+       </li>
+       <li>Accueil</li>
+       <li>Accueil</li>
+       <li>Accueil</li>
+       <li>Accueil</li>
+       <li>Accueil</li>
+      </ul>
+    </div>
+    <div id="box5" class="green">
+    <h2>Getting started</h2>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+        <span><</span>!DOCTYPE html<span>></span>
+    </div>
+    <div id="box6" class="red">
+        <img src="forkme.png" >
+    </div>
+</div>
+</body>
+</html>
